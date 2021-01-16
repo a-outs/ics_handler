02c15480151eb8542c3c8d86e22092dabaeb449b
Aidan Lee <meow123al@gmail.com>
2021-01-16 11:02:15 -0800

    Add test flask API code

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Python code for handling the .ics files
 
 1. Create virtual environment in root directory of project.
 2. `pip install -r requirements.txt` to install dependencies
+
+Start flask 
+
+1. Run project
+2. `flask run` in virtualenv
--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import flask
+from flask import Flask
+from datetime import datetime
+import re
+
+app = Flask(__name__)
 
 
-
+@app.route("/")
 def home():
-    return "Hello, Flask"
-    
+    return "Hello, Flask!"
+
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
+
+
+@app.route('/time')
+def get_current_time():
+    return {'time': datetime.now()}
