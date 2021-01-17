60ead7a9abe97616bb2cfc0ff21de917249c2463
Aidan Lee <meow123al@gmail.com>
2021-01-16 22:14:27 -0800

    convert argument parsing to dictionary for simplicity

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 api = Api(app)
 
 in_payload_args = reqparse.RequestParser()
-in_payload_args.add_argument("inputLinkData", type=str, help="Canvas calendar link required!", required=True)
-in_payload_args.add_argument("blacklistData", type=str, help="Blacklist required!", required=True)
-in_payload_args.add_argument("separateData", type=bool, help="Separation data required!", required=True)
-in_payload_args.add_argument("excludeEventsData", type=bool, help="Exclusion data link required!", required=True)
+in_payload_args.add_argument("postData", type=dict, help="postData not received.", required=True)
+# in_payload_args.add_argument("blacklistData", type=str, help="Blacklist required!", required=True)
+# in_payload_args.add_argument("separateData", type=bool, help="Separation data required!", required=True)
+# in_payload_args.add_argument("excludeEventsData", type=bool, help="Exclusion data link required!", required=True)
 
 link_as_string = []
 generated_cals = {"links": link_as_string}
@@ -20,9 +20,8 @@ generated_cals = {"links": link_as_string}
 class ProcessData(Resource):
     def post(self):
         args = in_payload_args.parse_args()
-        link_as_string.append(args['inputLinkData'])
-        link_as_string.append(args['blacklistData'])
-        return generated_cals
+        userdict = args['postData']  # dictionary that contains data sent from frontend
+        return generated_cals  # provide a dictionary with one key-value pair containing a list of calendar links
 
 
 api.add_resource(ProcessData, "/api/PostCal")
