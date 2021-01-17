a078b342a7b64f78e93f4abd5218cc094b0850b5
Aidan Lee <meow123al@gmail.com>
2021-01-16 16:47:39 -0800

    Adjust names in reqparse to match those of the JSON Post Request

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 api = Api(app)
 
 in_payload_args = reqparse.RequestParser()
-in_payload_args.add_argument("linkData", type=str, help="Canvas calendar link required!", required=True)
+in_payload_args.add_argument("inputLinkData", type=str, help="Canvas calendar link required!", required=True)
 in_payload_args.add_argument("blacklistData", type=str, help="Blacklist required!", required=True)
-in_payload_args.add_argument("separateData", type=bool, help="Separation data required!", required=True)
+in_payload_args.add_argument("seperateData", type=bool, help="Separation data required!", required=True)
 in_payload_args.add_argument("excludeEventsData", type=bool, help="Exclusion data link required!", required=True)
 
 
 class ProcessData(Resource):
     def post(self):
         args = in_payload_args.parse_args()
-        return {"calLink": args["linkData"]}
+        return {"calLink": args["inputLinkData"]}
 
 
 api.add_resource(ProcessData, "/api/PostCal")
