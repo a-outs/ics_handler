f0e7d003a0bbd432410ed2c1f06a8014c6e7e1f8
Aidan Lee <meow123al@gmail.com>
2021-01-16 21:23:50 -0800

    Fixed JSON serialization

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, fields, marshal_with
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, marshal
 from datetime import datetime
 import re
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,14 +13,20 @@ in_payload_args.add_argument("blacklistData", type=str, help="Blacklist required
 in_payload_args.add_argument("seperateData", type=bool, help="Separation data required!", required=True)
 in_payload_args.add_argument("excludeEventsData", type=bool, help="Exclusion data link required!", required=True)
 
+link_as_string = []
+generated_cals = {"links": link_as_string}
+
 
 class ProcessData(Resource):
     def post(self):
         args = in_payload_args.parse_args()
-        return {"calLink": args["inputLinkData"]}
+        link_as_string.append(args['inputLinkData'])
+        link_as_string.append(args['blacklistData'])
+        return generated_cals
 
 
 api.add_resource(ProcessData, "/api/PostCal")
+
 
 @app.route("/")
 def home():
