f559830a3121ed5ae4c41b5a35cbf056f4b24b05
joshjun010202@gmail.com <joshjun010202@gmail.com>
2021-01-17 18:47:34 +0900

    Implemented calendar splitting function, but bugs present

--- process_calendar.py
+++ process_calendar.py
@@ -1,10 +1,29 @@
 from icalendar import Calendar, Event
 import urllib.request
-import json
+import random
 
 def get_calendar(url):
     ics_file = urllib.request.urlopen(url).read()
     return ics_file
+
+def add_event_to_cal(old_event, cal):
+    event = Event()
+
+    summary = old_event.get('summary')
+    dt_stamp = old_event.get('dtstamp').to_ical()
+    start_date = old_event.get('dtstart').to_ical()
+    end_date = old_event.get('dtend').to_ical()
+    uid = old_event.get('uid')
+    url = old_event.get('url')
+
+    event['DTSTAMP'] = dt_stamp
+    event['UID'] = uid
+    event['DTSTART'] = start_date
+    event['DTEND'] = end_date
+    event['SUMMARY'] = summary
+    event['URL'] = url
+
+    cal.add_component(event)
 
 def exclude_all_non_assignments(file):
     file_in_text = Calendar.from_ical(file)
@@ -15,21 +34,7 @@ def exclude_all_non_assignments(file):
         event_string = 'event-calendar-event'
 
         if event_string not in uid:
-            event = Event()
-
-            summary = old_event.get('summary')
-            dt_stamp = old_event.get('dtstamp').to_ical()
-            start_date = old_event.get('dtstart').to_ical()
-            end_date = old_event.get('dtend').to_ical()
-            uid = old_event.get('uid')
-
-            event['DTSTAMP'] = dt_stamp
-            event['UID'] = uid
-            event['DTSTART'] = start_date
-            event['DTEND'] = end_date
-            event['SUMMARY'] = summary
-
-            cal.add_component(event)
+            add_event_to_cal(old_event, cal)
 
     return cal.to_ical()
 
@@ -40,35 +45,41 @@ def blacklist_events(file, blacklist):
     for old_event in file_in_text.walk('VEVENT'):
         summary = old_event.get('summary')
         if (blacklist not in summary):
-            event = Event()
-
-            dt_stamp = old_event.get('dtstamp').to_ical()
-            start_date = old_event.get('dtstart').to_ical()
-            end_date = old_event.get('dtend').to_ical()
-            uid = old_event.get('uid')
-
-            event['DTSTAMP'] = dt_stamp
-            event['UID'] = uid
-            event['DTSTART'] = start_date
-            event['DTEND'] = end_date
-            event['SUMMARY'] = summary
-
-            cal.add_component(event)
+            add_event_to_cal(old_event, cal)
         
     return cal.to_ical()
 
-def seperate_cal_by_course(file): # returns list
+def seperate_cal_by_course(file): 
     cal_dict = {}
 
     file_in_text = Calendar.from_ical(file)
     for event in file_in_text.walk('VEVENT'):
         course_url = event.get('url')
-        print(course_url)
-        '''
+        # Course id is a 6 digit number followed by include_contexts=course_ in the url
         course_id = course_url.split("include_contexts=course_")
-        course_id = course_id[1][:7]
-        print(course_id)
-        '''
+        course_id = course_id[1][:6]
+
+        if course_id not in cal_dict:
+            # Create a new calendar object for course id
+            cal_dict[course_id] = Calendar()
+            # Add that event to calendar
+            add_event_to_cal(event, cal_dict[course_id])
+        else:
+            add_event_to_cal(event, cal_dict[course_id])
+    
+    return cal_dict
+
+def generate_unique_number(list):
+    name_num = random.randint(10000, 99999)
+    if name_num not in list:
+        return name_num
+    else:
+        generate_unique_number(list)
+
+def write_file(name, calendar):
+    f = open(name, 'wb')
+    f.write(calendar)
+    f.close()
 
 def filter_calendar(dict):
     input_link = dict["inputLinkData"]
@@ -91,12 +102,39 @@ def filter_calendar(dict):
 
     # Take the filtered list from prev. and see if it needs to be split into seperate calendars
     if is_cal_seperated:
-        seperate_cal_by_course(file)
+        num_list = []
+        name_list = []
+        cal_dict = seperate_cal_by_course(file)
+
+        for key in cal_dict:
+            name_num = generate_unique_number(num_list)
+            num_list.append(name_num)
+
+            for num in num_list:
+                file_name = str(num) + ".ics"
+                name_list.append(file_name)
+                write_file(file_name, cal_dict[key].to_ical())
+        
+        return name_list
+
+'''
+def generate_num_list(): #return list
+    num_list = []
+    name_num = generate_unique_number(num_list)
+    num_list.append(name_num)
+
+    return num_list
+
+def generate_calender_files(num_list, key):
+    name_list = []
+    for num in num_list:
+        file_name = str(num) + ".ics"
+        write_file(file_name, cal_dict[key])
+'''
     
 
-    f = open('test4.ics', 'wb')
-    f.write(file)
-    f.close()
+
+
 
 given_dict = {"inputLinkData":"https://canvas.ucdavis.edu/feeds/calendars/user_URNeG1MSEjHo2ChpoCUFan9VQ4NDe15UE3bzMlhj.ics","blacklistData":"MAT 021B","seperateData":True,"excludeEventsData":True}
 filter_calendar(given_dict)
