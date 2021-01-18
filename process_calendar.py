29e6fcd1c8acf9d22e3ec857675a1dfa66dbbe79
joshjun010202@gmail.com <joshjun010202@gmail.com>
2021-01-18 10:21:37 +0900

    added peter's code

--- process_calendar.py
+++ process_calendar.py
@@ -177,5 +177,63 @@ def filter_calendar(dict):
         name_list.append(file_name)
         return name_list
 
-#given_dict = {"inputLinkData":"https://canvas.ucdavis.edu/feeds/calendars/user_URNeG1MSEjHo2ChpoCUFan9VQ4NDe15UE3bzMlhj.ics","blacklistData":"","separateData":True,"excludeEventsData":True, "startDate":"2021-01-01", "endDate":"2021-02-02"}
+# Peter's code
+def make_recurring(calendar):
+    hash_by_name = create_hash(calendar)
+    updated_cal = copy_start_cal(calendar)
+    for name in hash_by_name:
+        add_to_cal(updated_cal, hash_by_name[name])
+    return updated_cal
+
+
+def create_hash(calendar):
+    event_names = {}
+    for cur_event in calendar.walk('vevent'):
+        if cur_event['summary'] not in event_names:
+            event_names[cur_event['summary']] = [[cur_event, [(cur_event.get('dtstart')).dt]]]
+        else:
+            seven_days = datetime.timedelta(days=7)
+            event_list = event_names[cur_event['summary']]
+            test = 0
+            for i in range(len(event_list)):
+                if (((cur_event.get('dtstart')).dt - (
+                        event_list[i][0].get('dtstart')).dt) % seven_days == datetime.timedelta(seconds=0)):
+                    event_list[i][1].append((cur_event.get('dtstart')).dt)
+                    test = 1
+                    break
+            if test == 0:
+                event_list.append([cur_event, [(cur_event.get('dtstart')).dt]])
+    return event_names
+
+
+def copy_start_cal(calendar):
+    new_cal = Calendar()
+    new_cal.add('version', calendar['version'].to_ical())
+    new_cal.add('prodid', calendar['prodid'].to_ical())
+    new_cal.add('calscale', calendar['calscale'].to_ical())
+    new_cal.add('method', calendar['method'].to_ical())
+    return new_cal
+
+
+def add_to_cal(calendar, event_occur_list):
+    for i in range(len(event_occur_list)):
+        day = (event_occur_list[i][0].get('dtstart')).dt.strftime("%a")
+        day = day[: -1]
+        exdates = get_exdates(event_occur_list[i][1])
+        event_occur_list[i][0].add('rrule',
+                                   {'freq': 'weekly', 'wkst': 'su', 'byday': day, 'count': len(event_occur_list[i][1]) + len(exdates)})
+        event_occur_list[i][0].add('exdate', exdates)
+        calendar.add_component(event_occur_list[i][0])
+
+
+def get_exdates(datetime_list):
+    exdate_list = []
+    datetime_list.sort()
+    for i in range(len(datetime_list) - 1):
+        if datetime_list[i] - datetime_list[i + 1] != datetime.timedelta(days=-7):
+            date = datetime_list[i] + datetime.timedelta(days=7)
+            exdate_list.append(date)
+    return exdate_list
+
+#given_dict = {"inputLinkData":"link","blacklistData":"","separateData":True,"excludeEventsData":True, "startDate":"2021-01-01", "endDate":"2021-02-02"}
 #filter_calendar(given_dict)
